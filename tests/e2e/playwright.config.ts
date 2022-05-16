a7305907dd97ac40c70b5f3e72516d6f909f27bd
Yustinian Sizariev <98766650+ysizarie-virtru@users.noreply.github.com>
2022-05-16 13:34:11 +0300

    fix: playwright.config.ts (#1714) (#75)

--- tests/e2e/playwright.config.ts
+++ tests/e2e/playwright.config.ts
@@ -6,11 +6,11 @@ dotenv.config({ multiline: true });
 /* See https://playwright.dev/docs/test-configuration. */
 const config: PlaywrightTestConfig = {
   /* Fail the build on CI if you accidentally left test.only in the source code. */
-  forbidOnly: !!process.env.CI,
+  forbidOnly: Boolean(process.env.CI),
   /* Retry on CI only */
   retries: process.env.CI ? 2 : 0,
   /* Opt out of parallel tests on CI and Local env for now (due to test failures with multiple workers - PLAT-1774  */
-  workers: process.env.CI ? 1 : 1,
+  workers: 1,
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
     /* Maximum time each action such as `click()` can take. Defaults to 0 (no limit). */
@@ -21,7 +21,7 @@ const config: PlaywrightTestConfig = {
     trace: 'on-first-retry',
     screenshot: 'only-on-failure',
     browserName: "chromium",
-    headless: false,
+    headless: Boolean(process.env.CI),
     launchOptions: {
       slowMo: 50,
       // devtools: true,
@@ -37,7 +37,7 @@ const config: PlaywrightTestConfig = {
   /* Maximum time one test can run for. */
   timeout: 30 * 1000,
   //   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
-  reporter: 'html',
+  reporter: process.env.CI ? 'line' : 'html',
   //   /* Configure projects for major browsers */
   // projects: [
   //   {
